a6f2904c8052a1f99e11d279ca38eda827c02059
eclectic-Owl <dewell92@gmail.com>
2022-05-01 23:26:21 -0500

    tests push

---
+++ Group_3_Week_11_DB_APITests/Controllers/ClassesControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group_3_Week_11_DB_API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Group_3_Week_11_DB_API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group_3_Week_11_DB_API.Controllers.Tests
+{
+    [TestClass()]
+    public class ClassesControllerTests
+    {
+        private Wossamotta_UContext context;
+
+        [TestMethod()]
+        [ExpectedException(typeof(DbUpdateConcurrencyException))]
+        public void PutClass()
+        {
+            //PutClass badClass = new PutClass();
+
+
+
+        }
+        
+    }
+}
---
+++ Group_3_Week_11_DB_APITests/JwtAuthenticationManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group_3_Week_11_DB_API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Group_3_Week_11_DB_API.Controllers.AuthenticationController;
+using static Group_3_Week_11_DB_API.Controllers.ClassesController;
+using static Group_3_Week_11_DB_API.Controllers.StudentsController;
+namespace Group_3_Week_11_DB_API.Tests
+{
+    [TestClass()]
+    public class JwtAuthenticationManagerTests
+    {
+        [TestMethod()]
+        public void JwtAuthenticationManagerTest()
+        {
+            JwtAuthenticationManager manager = new JwtAuthenticationManager("fakepassword12345!");
+            User user = new User
+            {
+                Username = "TestUserFalse",
+                Password = "TestPasswordFalse!!!"
+            };
+
+            var ret = manager.Authenticate(user.Username, user.Password);
+            Assert.IsNull(ret);
+            Assert.IsNotNull(manager.Authenticate("test","password"));
+            
+        
+        }
+    }
+}
